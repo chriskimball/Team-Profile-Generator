d6953a07ed4ec1a90e88a36f1a9b489696fc3ad7
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-01-19 23:53:33 -0800

    Added questions to index.js and started building inquire functions

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-/* propmt Please build your team
+const inquirer = require('inquirer')
+
+/* prompt Please build your team
 ONE PROMPT 
     [
     Q1 What is the team Manager's name?
@@ -27,6 +29,81 @@ ANOTHER PROMPT (What type of team member would you like to add?)
     ]
 */
 
+const employees = [];
+
+
+const managerQuestions = [
+    {
+        type: "input",
+        message: "What is the team Manager's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the team manager's id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is the team manager's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the team manager's office number?",
+        name: "phone"
+    }
+]
+
+
+
+const engineerQuestions = [
+    {
+        type: "input",
+        message: "What is the engineer's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the engineer's id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is the engineer's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the engineer's github profile?",
+        name: "github"
+    }
+]
+
+const internQuestion = [
+    {
+        type: "input",
+        message: "What is the engineer's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the engineer's id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is the engineer's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your intern's School?",
+        name: "school"
+    }
+]
+
+
 
 // Pseudo code
 
@@ -34,30 +111,60 @@ ANOTHER PROMPT (What type of team member would you like to add?)
 
 // Ask them for manager info
 
+
+
+async function inquire (role) {
+    switch (role) {
+        case 'Engineer':
+            questions = engineerQuestions;
+            break;
+        case 'Intern':
+            questions = engineerQuestions;
+          break;
+        case 'Manager':
+            questions = managerQuestions;
+    const answers = await
     // Prompt the user for the data
     inquirer
-        .prompt(/* questions */)
+        .prompt(questions)
         .then((answers) => {
-
+            console.log(answers)
             // THEN create and store an object for the Manager
-            employees.push(new Manager ( /* pass in answer data from inquirer*/ ));
+            // employees.push(new Manager ( /* pass in answer data from inquirer*/ ));
             // THEN `Ask what they would like to do next`
             askForNextAction();
         })
-}
+};
 
-// same function above but async and await
-async function asyncAskForManager() {
-    const answers = await 
+
+
+async function askForNextAction(role) {
+    
     inquirer
-    .prompt( /* questions */ )
-
-    // THEN create and store an object for the Manager
-    employees.push(new Manager ( /* pass in answer data from inquirer*/ ));
-    // THEN `Ask what they would like to do next`
-    askForNextAction();
+        .prompt(questions)
+        .then((answers) => {
+            switch (role) {
+                case 'Engineer':
+                    questions = engineerQuestions;
+                    break;
+                case 'Intern':
+                    questions = engineerQuestions;
+                  break;
+                case 'Manager':
+                    questions = managerQuestions;
+            console.log(answers)
+            // THEN create and store an object for the Manager
+            // employees.push(new Manager ( /* pass in answer data from inquirer*/ ));
+            // THEN `Ask what they would like to do next`
+            askForNextAction();
+        })
+    
+    inquire(questions)
 }
 
+const init = (role) => inquire('Manager');
+
+init()
 
 
 // Ask them for engineer info
@@ -87,43 +194,48 @@ async function asyncAskForManager() {
 
 // Class constructor example
 
-class Employee {
-    constructor (name, id, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
+// class Employee {
+//     constructor (name, id, email) {
+//         this.name = name;
+//         this.id = id;
+//         this.email = email;
+//     }
 
-    getName () {
-        return this.name;
-    }
+//     getName () {
+//         return this.name;
+//     }
 
-    getId () {
-        return this.id
-    }
+//     getId () {
+//         return this.id
+//     }
 
-    getEmail () {
-        return this.email
-    }
+//     getEmail () {
+//         return this.email
+//     }
 
-    getRole () {
-        return `Employee`
-    }
-}
+//     getRole () {
+//         return `Employee`
+//     }
+// }
 
-// import/require employee for each sub class
-class Manager extends Employee {
-    constructor(role, phone) {
-      this.role = role
-      this.phone = phone 
-    //   const area = sideA * sideB;
-    //   const perimeter = sideA * 2 + sideB * 2;
+// // import/require employee for each sub class
+// class Manager extends Employee {
+//     constructor(role, phone) {
+//       this.role = role
+//       this.phone = phone 
+//     //   const area = sideA * sideB;
+//     //   const perimeter = sideA * 2 + sideB * 2;
   
-      super(name, id, email);
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
-}
+//       super(name, id, email);
+//         this.name = name;
+//         this.id = id;
+//         this.email = email;
+//     }
+// }
 
-// Export after constructing the new class/subclass
+
+// // Export after constructing the new class/subclass
+
+// const manager = new Manager('manager', '112')
+// console.log(manager)
+
